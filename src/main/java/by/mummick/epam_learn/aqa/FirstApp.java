c0c3e5816a962008a4cea5f09feb0b603db43ad5
Dzmitry Mamaikin <mummick6@gmail.com>
2019-06-07 15:28:17 +0300

    First app add comment

--- src/main/java/by/mummick/epam_learn/aqa/FirstApp.java
+++ src/main/java/by/mummick/epam_learn/aqa/FirstApp.java
@@ -1,5 +1,6 @@
 package by.mummick.epam_learn.aqa;
 
+//First app. Code from lection
 public class FirstApp {
     public static void main(String[] args) {
         System.out.println("hello, Java");
