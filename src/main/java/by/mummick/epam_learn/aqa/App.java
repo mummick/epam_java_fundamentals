c1f536a84dea411a6136a0b0a9f25c5249c97655
Dzmitry Mamaikin <mummick6@gmail.com>
2019-06-07 14:49:20 +0300

    initialize repository

---
+++ src/main/java/by/mummick/epam_learn/aqa/App.java
@@ -0,0 +1,13 @@
+package by.mummick.epam_learn.aqa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
