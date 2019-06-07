3e465fa022d4eb8122afa5948fef5ea8d1520cf2
Dzmitry Mamaikin <mummick6@gmail.com>
2019-06-07 15:21:29 +0300

    Add first application

---
+++ src/main/java/by/mummick/epam_learn/aqa/FirstApp.java
@@ -0,0 +1,9 @@
+package by.mummick.epam_learn.aqa;
+
+public class FirstApp {
+    public static void main(String[] args) {
+        System.out.println("hello, Java");
+        System.out.println(42);
+        System.out.println("\n\tjava " + 11);
+    }
+}
